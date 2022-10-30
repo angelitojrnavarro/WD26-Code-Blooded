b1e79cc8f7c82c82ceea648907a337a62419001a
Angelito Navarro Jr <angelitojr.navarro@gmail.com>
2022-10-30 23:05:29 +0800

    first commit

---
+++ Activity01/js/index.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+AOS.init();
