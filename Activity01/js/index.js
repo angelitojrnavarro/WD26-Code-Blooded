16aeb8671c962207a95f49074a5a8d10f8d1115c
Angelito Navarro Jr <angelitojr.navarro@gmail.com>
2022-11-03 20:56:44 +0800

    first commit

--- Activity01/js/index.js
+++ Activity01/js/index.js
@@ -16,3 +16,28 @@ $('.owl-carousel').owlCarousel({
 })
 
 AOS.init();
+
+
+
+let previewContainer = document.querySelector('.product-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.item .card').forEach(card =>{
+    card.onclick = () =>{
+        previewContainer.style.display = 'flex';
+        let name = card.getAttribute('data-name');
+        previewBox.forEach(preview =>{
+            let target = preview.getAttribute('data-target');
+            if(name == target){
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close =>{
+    close.querySelector('.fa-times').onclick = () =>{
+        close.classList.remove('active');
+        previewContainer.style.display ='none';
+    };
+});
---
+++ s12/script.js
@@ -0,0 +1,53 @@
+/*  Javascript
+    - creates dynamically updating content, control multimedia, animate images, and pretty much everything else.
+
+
+    server side - ability to update the display
+
+    client side - genarates new content inside client browser
+
+*/
+/*
+//console.log("hello word!");
+
+
+//1. Using InnerHTML
+//document.getElementById("sample").innerHTML = ("Welcome to javascript");
+// the most common way to display data in HTML
+
+//2. Document.write() - for testing porpose 
+//document.write("hello method")
+
+//3. alert() - display a window in the web browser.
+
+alert("I am an Alert!");
+
+//4. console.log() - for debugging purposes
+//console.log("This is a console log");
+
+*/
+//this is for single line comment shortcut ctrl + /
+
+/*  mutli-line comment - crtl + shift + /   */
+
+/*
+3 types of Variable
+
+var - most common variable. 
+
+const - 
+
+let - 
+*/
+
+
+const pi = 3.1416;
+
+console.log(pi);
+
+let myCopy;
+console.log(myCopy);
+myCopy = "test";
+
+let name = 'tolits';
+console.log(name);
